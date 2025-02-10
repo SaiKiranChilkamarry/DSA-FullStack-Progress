ba18db8a56365a18c96f46f8caaba92102446199
Sai Kiran Chilkamarry <sai333408@gmail.com>
2025-02-10 22:42:04 +0530

    Added basic array implimentation in Python

---
+++ DSA/Arrays/arrays.py
@@ -0,0 +1,11 @@
+#create an array 
+array=[1,2,3,4,5]
+
+#inserting an element at end of the array
+array.append(6)
+
+#deletion 
+array.remove(3)
+
+#accessing the element 
+print ("Element at inex 2:",arr[2])
