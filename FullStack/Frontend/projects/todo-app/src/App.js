d18e1339d9347dcf6f9a8c28610d12e699d87349
Sai Kiran Chilkamarry <sai333408@gmail.com>
2025-02-15 20:42:02 +0530

    Added input field, button, and functionality to add tasks using useState

--- FullStack/Frontend/projects/todo-app/src/App.js
+++ FullStack/Frontend/projects/todo-app/src/App.js
@@ -1,10 +1,37 @@
+import React,{useState} from 'react';
 function App(){
+  const [task,setTask]=useState("");//to store input values
+  const[tasks,setTasks]=useState([])//to keep track of list of task user added
+
+  //implimenting adding the task  when add task button clicked
+  const addTask =()=>{
+    setTasks([...tasks,task]);//Adding of new task
+    setTask("");//clear the input feild
+  };
+
   return (
     <div className="App">
       <h1>
         To-Do App
       </h1>
+      <input
+        type = "text"
+        placeholder="Enter task..."
+        value={task}
+        onChange={(e)=>setTask(e.target.value)}
+        style={{padding:"10px",margin:"10px",width:"200px"}}
+      />
+      <button onClick={addTask}  style={{padding:"10px",margin:"10px"}}>Add Task</button>
+      <ul>
+        {tasks.map((task,index)=>(
+          <li key={index}>
+            {task}
+          </li>//display each task
+        ))}
+      </ul>
     </div>
   );
+  
+
 }
 export default App;
